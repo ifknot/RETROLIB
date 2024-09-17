f26d6b755c8f0a84dd80619a8707a0a809c11892
ifknot <ifknot@hotmail.com>
2024-09-17 21:29:07 +0100

    main

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @file      main.c
+ *  @brief     RETROLIB
+ *  @details   DOS Game Library
+ *  @author    Jeremy Thornton
+ *  @date      17.09.2024
+ *  @copyright © Jeremy Thornton, 2024. All right reserved.
+ *
+ */
+
+int main(int argc, char* argv[]) {
+
+}
