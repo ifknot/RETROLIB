df83d0fe7d54ddbb2a42624e7320ade49add6569
ifknot <ifknot@hotmail.com>
2024-09-19 08:47:39 +0100

    TODO main.c

--- main.c
+++ main.c
@@ -5,12 +5,21 @@
  *  @details   DOS Game Library
  *  @author    Jeremy Thornton
  *  @date      17.09.2024
- *  @copyright © Jeremy Thornton, 2024. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2024. All right reserved.
  *
  */
 
 #include "JOT/jot_macros.h"
 
+/**
+* TODO:
+* [ ] Q10_6_t 
+* [ ] Q vectors
+* [ ] actor model
+* [ ] HGA plot
+* [ ] particles 
+* [ ] gravity
+*/
 int main() {
 
 	//REDIRECT_STDERR("log.txt");
