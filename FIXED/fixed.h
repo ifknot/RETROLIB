f8fdcd216989c4deb743027359dec8e66b54a899
ifknot <ifknot@hotmail.com>
2024-09-20 22:04:07 +0100

    start fixed

---
+++ FIXED/fixed.h
@@ -0,0 +1,14 @@
+/**
+ *
+ *  @brief     
+ *  @details   ~
+ *  @author    Jeremy Thornton
+ *  @date      20.09.2024
+ *  @copyright © Jeremy Thornton, 2024. All right reserved.
+ *
+ */
+#ifndef FIXED_H
+#define FIXED_H
+
+
+#endif
