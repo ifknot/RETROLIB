61dafd898b5dc71d272da01412c68877b3ac3527
ifknot <ifknot@hotmail.com>
2024-09-30 16:13:52 +0100

    Update bios_memory_size.c

--- BIOS/bios_memory_size.c
+++ BIOS/bios_memory_size.c
@@ -5,7 +5,7 @@
  *  @details   ~
  *  @author    Jeremy Thornton
  *  @date      22.11.2023
- *  @copyright © Jeremy Thornton, 2023. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2023. All right reserved.
  *
  */
 #include "bios_memory_size.h"
@@ -14,8 +14,14 @@ uint16_t bios_memory_size_KiB() {
 	uint16_t size_kib;
 	__asm {
 		.8086
+		pushf                                ; preserve what int BIOS functions may not
+        	push    ds                           ; due to unreliable behaviour
+	
 		int		BIOS_REPORT_MEMORY_SIZE
 		mov		size_kib, ax
+
+		pop 	ds 
+		popf
 	}
 	return size_kib;
 }
