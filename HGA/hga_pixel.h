23b7c0853f82f726547e0911b4bc6340c9c1f227
ifknot <ifknot@hotmail.com>
2024-10-03 11:34:46 +0100

    Update hga_pixel.h

--- HGA/hga_pixel.h
+++ HGA/hga_pixel.h
@@ -1,3 +1,4 @@
+/*
 graphics mode row table macro
 
 n = 0
@@ -12,3 +13,34 @@ n++
 loop
 
 HGA does not require await vertical retrace 
+*/
+
+// use clipping routines to massage *before* calling graphics functions
+
+#include <stdint.h>
+
+void hga_plot_pixel_lookup_table(uint16_t buffer, uint16t x, uint16_t y, uint8_t colour, uint16_t* row_table);
+
+void hga_plot_pixel_calculate(uint16_t buffer, uint16t x, uint16_t y, uint8_t colour);
+
+void hga_plot_pixel_mul_jump(uint16_t buffer, uint16t x, uint16_t y, uint8_t colour);
+
+uint8_t hga_read_buffer_byte(uint16_t buffer, uint16t x, uint16_t y);
+
+uint8_t hga_read_pixel_lookup_table(uint16_t buffer, uint16t x, uint16_t y, uint16_t* row_table);
+
+inline void hga_plot_pixel(uint16t x, uint16_t y) {
+  hga_plot_pixel_lookup_table(HGA_BUFFER_0, x, y, HGA_WHITE);
+  // plot_pixel_calculate(HGA_BUFFER_0, x,y,HGA_WHITE);
+  // plot_pixel_mul_jump(HGA_BUFFER_0, x,y,HGA_WHITE);
+}
+
+inline void hga_unplot_pixel(uint16t x, uint16_t y) {
+  hga_plot_pixel_lookup_table(HGA_BUFFER_0, x, y, HGA_BLACK, &ROW_TABLE);
+  // plot_pixel_calculate(HGA_BUFFER_0, x,y,HGA_BLACK);
+  // plot_pixel_mul_jump(HGA_BUFFER_0, x,y,HGA_BLACK;
+}
+
+inline uint8_t hga_read_pixel(uint16t x, uint16_t y) {
+  return hga_read_pixel_lookup_table(HGA_BUFFER_0, x, y, &ROW_TABLE); // uses hga_read_buffer_byte(buffer, x, y) mechanics
+}
