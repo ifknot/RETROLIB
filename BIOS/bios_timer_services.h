c17aef846897ddd29d7027ece59aaa6208ff500b
ifknot <ifknot@hotmail.com>
2024-10-07 13:45:03 +0100

    Create bios_timer_services.h

---
+++ BIOS/bios_timer_services.h
@@ -0,0 +1,13 @@
+
+
+/**
+*  INT 1aH: Timer I/O (and MRCI POST Hook)
+ INT 1aH 00H: Read System Clock (Ticks)
+ INT 1aH 01H: Set System Clock (Ticks)
+ INT 1aH 02H: Read Time from Real-Time Clock
+ INT 1aH 03H: Set Time on Real-Time Clock
+ INT 1aH 04H: Read Date from Real-Time Clock
+ INT 1aH 05H: Set Date on Real-Time Clock
+ INT 1aH 06H: Set Real-Time Clock Alarm
+ INT 1aH 07H: Cancel Real-Time Clock Alarm
+*/
