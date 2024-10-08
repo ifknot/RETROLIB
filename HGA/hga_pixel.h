32ac12a584842a041bd85daf178c64a17230cfd5
ifknot <ifknot@hotmail.com>
2024-10-08 10:26:44 +0100

    Update hga_pixel.h

--- HGA/hga_pixel.h
+++ HGA/hga_pixel.h
@@ -4,7 +4,7 @@
  *  @details   ~
  *  @author    Jeremy Thornton
  *  @date      6.10.2024
- *  @copyright © Jeremy Thornton, 2024. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2024. All right reserved.
  *
  */
 #ifndef HGA_PIXEL_H
@@ -17,12 +17,12 @@
 #include "hga_constants.h"
 #include "hga_colours.h"
 
-void hga_plot_pixel_lookup_table(uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t c, uint16_t* row_table);
+void hga_plot_pixel_lookup_table(uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t c);
 
 void hga_plot_pixel_calculate(uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t c);
 
-uint8_t hga_read_buffer_byte_lookup_table(uint16_t vram_segment, uint16_t x, uint16_t y);
+uint8_t hga_read_buffer_byte(uint16_t vram_segment, uint16_t x, uint16_t y);
 
-uint8_t hga_read_pixel_lookup_table(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t* row_table);
+uint8_t hga_read_pixel(uint16_t vram_segment, uint16_t x, uint16_t y);
 
 #endif
