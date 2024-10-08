e3f02ae3f3f12f821f9c7759c92fbed030a9cc11
ifknot <ifknot@hotmail.com>
2024-10-08 09:55:13 +0100

    Update math_prng.c

--- MATH/math_prng.c
+++ MATH/math_prng.c
@@ -6,6 +6,9 @@
 /**
 * @note the initial seeding value must not be zero
 */
+ x ^= x << 7;
+*    x ^= x >> 9;
+*    x ^= x << 8;
 static uint32_t _seed = 0xDECAFBAD;
 
 uint16_t math_prng_xorshift16() {
@@ -13,25 +16,25 @@ uint16_t math_prng_xorshift16() {
         .8086
         push    ds 
 
-        les     ax, _seed
+        les     ax, _seed          ; ES:AX = 32 bit seed 
 
-        mov     bx, ax 
+        mov     bx, ax             ; use 16 bit seed
         mov     cl, 7
-        shl     bx, cl 
-        xor     ax, bx 
+        shl     bx, cl             ; x << 7
+        xor     ax, bx             ; x ^= x << 7
 
         mov     bx, ax
         mov     cl, 9
-        shr     bx, cl
-        xor     ax, bx
+        shr     bx, cl            ; x >> 9
+        xor     ax, bx            ; x ^= x >> 9
 
         mov     bx, ax
         mov     cl, 8
-        shl     bx, cl
-        xor     ax, bx
+        shl     bx, cl            ; x << 8
+        xor     ax, bx            ; x ^= x << 8
 
-        lea     bx, _seed
-        mov     [bx], ax
+        lea     di, _seed
+        mov     [di], ax          ; update seed value
 
         pop     ds
     }
@@ -40,15 +43,44 @@ uint16_t math_prng_xorshift16() {
 }
 
 uint16_t math_prng_range_xorshift16(uint16_t min, uint16_t max) {
-    /*
-    Another note is that there is more easy way to get the number in the needed interval :
+    uint16_t rand = 0;
+    __asm {
+        .8086
+        push    ds
+    
+        les     ax, _seed          ; ES:AX = 32 bit seed 
 
-    mov     ax, dx
-    xor     dx, dx
-    mov     bx, max - min
-    div     bx              ; DX remainder(modulus)
-    return 0;
-    */
+        mov     bx, ax             ; use 16 bit seed
+        mov     cl, 7
+        shl     bx, cl             ; x << 7
+        xor     ax, bx             ; x ^= x << 7
+
+        mov     bx, ax
+        mov     cl, 9
+        shr     bx, cl            ; x >> 9
+        xor     ax, bx            ; x ^= x >> 9
+
+        mov     bx, ax
+        mov     cl, 8
+        shl     bx, cl            ; x << 8
+        xor     ax, bx            ; x ^= x << 8
+
+        lea     di, _seed
+        mov     [di], ax          ; update seed value
+
+        mov     bx, max           ; calculate range 
+        sub     bx, min           ; BX = range
+
+        xor     dx, dx            ; prepare DX use in division
+        div     bx                ; DX:AX / BX 
+        add     dx, min           ; DX is the modulus + start of the range
+
+        lea     di, rand
+        mov     [di], dx          
+        
+        pop     ds
+    }
+    return  rand;
 }
 
 void math_prng_set_seed(uint32_t s) {
