0a5d4c2419daf1a8bece9fb7408619e562c24b12
ifknot <ifknot@hotmail.com>
2024-10-10 15:28:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 // #include "DEMOS/TEXT2PIX.h"
 #include "HGA/demo_hga.h"
 
+// TODO:
+// [ ] finish buffer demo 
+// [ ] demo buffer png anim to git readme 
+// [ ] pixel perf compare algos 
+// [ ] demo Mendelbrot fixed point
+// [ ] bresenham line 
+// [ ] demo lines
+// [ ] rectangle 
+// [ ] demo rectangle
+// [ ] circle 
+// [ ] demo circle
+// [ ] byte aligned block transfer
+// [ ] demo bytes_aligned_blt
 
 int main() {
 
