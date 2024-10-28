1b0c272ece2186d30f99d2d6f0a5f0f4d89a8d44
ifknot <ifknot@hotmail.com>
2024-10-28 10:53:18 +0000

    Update hga_video_mode.h

--- HGA/hga_video_mode.h
+++ HGA/hga_video_mode.h
@@ -4,7 +4,7 @@
  *  @details   ~
  *  @author    Jeremy Thornton
  *  @date      29.09.2024
- *  @copyright © Jeremy Thornton, 2024. All right reserved.
+ *  @copyright Â© Jeremy Thornton, 2024. All right reserved.
  *
  */
 #ifndef HGA_VIDEO_MODE_H
@@ -14,6 +14,7 @@
 
 #include "hga_constants.h"
 #include "hga_video_mode_presets.h"
+//#include "hga_table_video_modes.h"
 
 /**
 *  @brief  program CRTC for video mode as described by the CRTC presets data table.
@@ -27,15 +28,15 @@ void hga_video_mode(const uint8_t* presets, uint8_t flags_config, uint8_t flags_
 /**
 *  @brief  enter text "half" mode single display frame (mimic MDA).
 */
-void mda_text_mode() {
-    hga_video_mode(hga_crtc_presets_text_mode, hga_text_config_default, hga_text_ctrl_default);
+inline void mda_text_mode() {
+    hga_video_mode(hga_crtc_presets_text_mode, hga_text_config_default, hga_text_ctrl_default); // HGA_TABLE_TEXT_MODE_DEFAULTS
 }
 
 /**
 *  @brief  enter Hercules graphics "full" mode with 2 frame buffers.
 */
-void hga_graphics_mode() {
-    hga_video_mode(hga_crtc_presets_graphics_mode, hga_graphics_config_default, hga_graphics_ctrl_default);
+inline void hga_graphics_mode() {
+    hga_video_mode(hga_crtc_presets_graphics_mode, hga_graphics_config_default, hga_graphics_ctrl_default); // HGA_TABLE_GRAPHICS_MODE_DEFAULTS
 }
 
 #endif
