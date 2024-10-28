e370eb82197799b8e5731135506ca091d2772534
ifknot <ifknot@hotmail.com>
2024-10-28 09:17:24 +0000

    Update hga_display_buffer.h

--- HGA/hga_display_buffer.h
+++ HGA/hga_display_buffer.h
@@ -19,6 +19,13 @@
 void hga_select_display_buffer(uint8_t select);
 
 /**
+* @brief Write an 8 bit byte pattern at (byte, line) coordinates x(0..89), y(0..347) to the specified HGA VRAM buffer
+* @details
+* @note No sanity checking!
+*/
+void hga_write_vram_buffer((uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t byte_pattern);
+
+/**
 * @brief Fast fill the specified HGA VRAM buffer with an 8 bit byte pattern
 * @details
 * @note No sanity checking! Expects vram_segment to be valid.
