4fc66715ed300cc6ec753407a40b66199bb26e11
ifknot <ifknot@hotmail.com>
2024-10-28 09:34:31 +0000

    Update hga_display_buffer.c

--- HGA/hga_display_buffer.c
+++ HGA/hga_display_buffer.c
@@ -28,11 +28,11 @@ J1:   out     dx, al
     }
 }
 
-void hga_write_vram_buffer((uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t byte_pattern) {
+void hga_write_vram_buffer(uint16_t vram_segment, uint16_t x, uint16_t y, uint8_t byte_pattern) {
 
 }
 
-uint8_t hga_read_vram_buffer((uint16_t vram_segment, uint16_t x, uint16_t y) {
+uint8_t hga_read_vram_buffer(uint16_t vram_segment, uint16_t x, uint16_t y) {
 	return 0;
 }
 
