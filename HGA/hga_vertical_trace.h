f6a1092d5bedbaee958b4ff76fd949cbb8846678
ifknot <ifknot@hotmail.com>
2024-11-14 19:47:49 +0000

    convert to CMAKE and Zed

--- HGA/hga_vertical_trace.h
+++
@@ -1,8 +0,0 @@
-/**
-* HGA does not require vertical retrace timing for flicker free animation - unlike CGA cards.
-* 
-* However, there are occasions where large changes to the display eg swapping display buffers can benefit aesthetically 
-* from timing such actions to occur in time with the vertical retrace.
-*/
-
-void hga_await_vertical_retrace();
