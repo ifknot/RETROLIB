1e919246460dad531359b22dd03ff946014ef845
ifknot <ifknot@hotmail.com>
2024-12-20 13:51:42 +0000

    Update pixelate.c

--- src/DEMOS/pixelate.c
+++ src/DEMOS/pixelate.c
@@ -14,23 +14,54 @@
 #include "../DOS/dos_tools_files.h"
 #include "../MEM/mem_arena.h"
 
-#define USAGE_INFO     "Converts a text file's characters to white pixels, punctuation to black pixels and newlines to a new pixel row."
-#define USAGE_FMT      "%s [inputfile] [outputfile]"
-#define ERR_GRAPHICS   "ERROR: This version of PIXELATE requires a Hercules Graphics Adapter."
+// string defines
+#define USAGE_INFO         "Converts a text file's characters to white pixels, punctuation to black pixels and newlines to a new pixel row."
+#define USAGE_FMT          "%s [inputfile] [outputfile]"
+#define ERR_GRAPHICS       "ERROR: This version of PIXELATE requires a Hercules Graphics Adapter."
+#define ERR_FOPEN_INPUT    ""
+#define ERR_FOPEN_OUTPUT   ""
+// value defines
 #define FILE_BLOCK_SIZE 720
+
 
 int pixelate(int argc, char** argv) {
     char file_path[255];
     char* text_buffer;
     dos_file_handle_t fhandle = 0;
     mem_arena_t* arena;
-    uint8_t card_detected, pixel_byte, pixel_bitmask;
+    uint8_t pixel_byte, pixel_bitmask;
     uint16_t file_bytes_read, byte_count, bit_count, k;
     uint32_t char_count = 0;
     bios_ticks_since_midnight_t t1, t2;
 
-// 1. minimal user input checking
-}
-  
-  return EXIT_SUCCESS;
+// 1. confirm appropriate graphics adapter present
+    if (!hga_detect_adapter()) {
+        fprintf(stderr, ERR_GRAPHICS);
+        return EXIT_FAILURE;
+    }
+
+// 2. minimal user input checking
+    if (argc != 2) {
+        fprintf(stderr, USAGE_INFO);
+        fprintf(stderr, USAGE_FMT, argv[0]);
+        return EXIT_FAILURE;
+    }
+// 3.0 extract the file name given by user from the arguement list
+    sscanf(argv[1], "%s", file_path);
+// 3.1 try open the file
+    fhandle = dos_open_file_using_handle(file_path, ACCESS_READ_ONLY);
+    if (!fhandle) {
+        INFO("fail to find");
+        return EXIT_FAILURE;
+    }
+// 3.2 total characters to process
+    char_count = dos_tools_file_size(fhandle));
+// 4.0 create screen size block of memory space as an arena
+    arena = mem_arena_new(MEM_ARENA_POLICY_DOS, FILE_BLOCK_SIZE);
+    if (!arena) {
+        return EXIT_FAILURE;
+    }
+// 4.1 allocate all of the arena as a char buffer
+    text_buffer = (char*)mem_arena_alloc(arena, FILE_BLOCK_SIZE);
+    return EXIT_SUCCESS;
 }
