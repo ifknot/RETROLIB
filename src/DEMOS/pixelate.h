bebd371677d3b07ac7d3989b669e3a5c0cfcbd51
ifknot <ifknot@hotmail.com>
2024-12-23 11:16:53 +0000

    Update pixelate.h

--- src/DEMOS/pixelate.h
+++ src/DEMOS/pixelate.h
@@ -1,12 +1,20 @@
-#ifndef PIXELATE_H
-#define PIXELATE_H
-
 /**
 * @brief    Demonstrates using retrolib libraries and the coding idioms for them
 * @details  Code as a Creative Medium 001
 * Visualize the patterns in text layout of a document by condensing characters into pixels.
 * Converts a text file's characters to white pixels, punctuation to black pixels and newlines to blank rows of pixels.
 * HGA is 720 x 348 pixels, so read upto 720 bytes from the text file at a time and then convert to pixels
+*/
+#ifndef PIXELATE_H
+#define PIXELATE_H
+
+/** 
+* test if the arguement is a character to be represented by a pixel?
+*/
+int is_pixel_character(char c);
+
+/** 
+* pixelate a text file according to decision function above
 */
 int pixelate(int argc, char** argv);
 
