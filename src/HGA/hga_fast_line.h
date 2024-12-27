d99abe59fd48e33ac59caea757698144bd146dbf
ifknot <ifknot@hotmail.com>
2024-12-27 15:10:39 +0000

    Update hga_fast_line.h

--- src/HGA/hga_fast_line.h
+++ src/HGA/hga_fast_line.h
@@ -2,13 +2,13 @@
 #define HGA_FAST_LINE_H
 
 /**
-* starting at screen coord (x,y) draws a horizontal line to point (a, y) ignoring b arguement
+* starting at screen coord (x1,y1) draws a horizontal line to point (x2, y2) ignoring b arguement
 */
-void hga_fast_hline(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t a, uint16_t b, uint8_t colour);
+void hga_fast_hline(uint16_t vram_segment, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t colour);
 
 /**
-* starting at screen coord (x,y) draws a vertical line to point (x,b) ignoring a arguement
+* starting at screen coord (x1,y1) draws a vertical line to point (x2,y2) ignoring a arguement
 */
-void hga_fast_vline(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t a, uint16_t b, uint8_t colour);
+void hga_fast_vline(uint16_t vram_segment, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t colour);
 
 #endif
