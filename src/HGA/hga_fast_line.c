4725b7409181f081b12e1567918b855418aec98c
ifknot <ifknot@hotmail.com>
2024-12-27 12:03:52 +0000

    Create hga_fast_line.c

---
+++ src/HGA/hga_fast_line.c
@@ -0,0 +1,9 @@
+#include "hga_fast_line.h"
+
+void hga_fast_hline(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t a, uint16_t b, uint8_t colour) {
+
+}
+
+void hga_fast_vline(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t a, uint16_t b, uint8_t colour) {
+
+}
