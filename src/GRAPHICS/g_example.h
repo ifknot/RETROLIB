248babc67147b825588f115803e61aed03d087e9
ifknot <ifknot@hotmail.com>
2025-01-22 12:27:09 +0000

    Update and rename g_rectangle.h to g_example.h

--- src/GRAPHICS/g_example.h
+++ src/GRAPHICS/g_example.h
@@ -3,19 +3,18 @@
 
 #inclde <stdint.h>
 
-#include "../HGA/hga.h"
+#include "../HGA/hga_g_functions.h"
 
-#include "g_types.h" 
-#include "g_boundry_handlers.h" 
 /*   For a function that returns an int and takes a char you would do
     typedef int (*functiontype2)(char);
     and to use it
     int dosomethingwithchar(char a) { return 1; }
-*/
+
 
 functiontype2 func2 = &dosomethingwithchar
 int result = func2('a');
 
-void g_draw_rectangle(g_rectangle_t* r, );
+void g_draw_rectangle(g_rectangle_t* r, ...);
+*/
 
 #endif
