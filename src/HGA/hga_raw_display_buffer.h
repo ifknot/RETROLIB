5a28343ceed9822cd49b73b7368a1cd922fce072
ifknot <ifknot@hotmail.com>
2025-01-22 16:17:38 +0000

    Update hga_raw_display_buffer.h

--- src/HGA/hga_raw_display_buffer.h
+++ src/HGA/hga_raw_display_buffer.h
@@ -1,3 +1,10 @@
+/**
+* C is an unconstrained language and most of its security is built around setting conventions.
+* @note UNSAFE none of the function arguements are sanity checked, processed, coded, formatted, or analyzed for safety
+* They are, however, fast.
+* @brief the "raw" family of low level graphics adapter specific graphics functions do no checking on their arguements!
+* It is expected that higher level calling function(s) will sanitize and bounds check any arguements to these low level functions.
+*/
 #ifndef HGA_DISPLAY_BUFFER_H
 #define HGA_DISPLAY_BUFFER_H
 
