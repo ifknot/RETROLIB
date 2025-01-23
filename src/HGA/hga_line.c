d0b0e6273e143835b317348c7386fc90a48d70df
ifknot <ifknot@hotmail.com>
2025-01-23 11:11:52 +0000

    Update hga_line.c

--- src/HGA/hga_line.c
+++ src/HGA/hga_line.c
@@ -43,9 +43,9 @@ void hga_hline(uint16_t vram_segment, uint16_t x1, uint16_t y1, uint16_t x2, uin
 Z0:	    jcxz    J0                                          ; lhs and rhs share same byte?
         dec     cx
         jcxz    J1                                          ; lhs and rhs share same word?
-        // 7.1 general case
+        // 7.1.0 general case
 		not 	dx											; convert proto-mask to mask word
-		// 7.2 colour lhs and rhs line
+		// 7.1.1 colour lhs and rhs line
 		add 	di, bx										; have ES:DI point to lhs
 		and     es:[di], dl                            		; mask out target bits 	- 16 + EA(8)
 		or      es:[di], al                            		; colour target bits	- 16 + EA(8)
@@ -53,32 +53,32 @@ Z0:	    jcxz    J0                                          ; lhs and rhs share 
 		inc 	di											; next byte
 		and     es:[di + bx], dh                            ; mask out target bits 	- 16 + EA(8)
 		or      es:[di + bx], ah                            ; colour target bits	- 16 + EA(8)
-		// 7.3 work out fill 'colour'
+		// 7.1.2 work out fill 'colour'
 		mov 	al, colour
 		mov 	ah, al
 		test 	al, al
 		jz 		Z1
 		mov     ax, 0FFFFh                                   ; AX white
-Z1:     // 7.4 handle odd or even line lengths
+Z1:     // 7.1.3 handle odd or even line lengths
 		cld                                                  ; clear direction flag
 		shr     cx, 1		                                 ; number of words to fill, lsb -> carry flag
 		jnc     NC0                                          ; even so no byte to fill
 		stosb	                                             ; odd do one byte al 'colour'
 		jcxz    END
-NC0:	// remaining word(s) ax 'colour'
+NC0:	// 7.1.4 remaining word(s) ax 'colour'
 		rep     stosw		                                 ; CX is checked for !=0 before even the first step
         jmp 	END
-J1:		// 8.0 special case same word (saves 48 clock cycles on 8086 line lengths 2 - 15)
+J1:		// 7.2.0 special case same word (saves 48 clock cycles on 8086 line lengths 2 - 15)
         not     dx                                           ; convert proto-mask to mask word
-        // 8.1 colour the shared lhs:rhs word                                       Clock Cycles
+        // 7.2.1 colour the shared lhs:rhs word                                       Clock Cycles
         and     es:[di + bx], dx                            ; mask out target word 	- 16 + EA(8)
 		or      es:[di + bx], ax                            ; colour target word	- 16 + EA(8)
 		jmp 	END
-J0:     // 9.1 special case same byte (saves 48 clock cycles on 8086 line lengths 0 - 7)
+J0:     // 7.3.0 special case same byte (saves 48 clock cycles on 8086 line lengths 0 - 7)
 		and     dl, dh                                      ; combine proto-mask into dl
 		not     dl		                                    ; convert proto-mask to mask
 		and     al, ah                                      ; combine 'colour' bits into al
-        // 9.2 colour the combined lhs&rhs byte										Clock Cycles
+        // 7.3.1 colour the combined lhs&rhs byte										Clock Cycles
 		and     es:[di + bx], dl                            ; mask out target bits 	- 16 + EA(8)
 		or      es:[di + bx], al                            ; colour target bits	- 16 + EA(8)
 END:
