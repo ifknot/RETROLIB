7bbe2e32c95688d04ba85eca88a1ba39f26d7d46
ifknot <ifknot@hotmail.com>
2025-01-28 11:06:54 +0000

    Update hga_filled_rectangle.h

--- src/HGA/hga_filled_rectangle.h
+++ src/HGA/hga_filled_rectangle.h
@@ -1,6 +1,6 @@
 #ifndef HGA_FILLED_RECTANGLE_H
 #define HGA_FILLED_RECTANGLE_H
 
-void hga_filled_rectangle():
+void hga_filled_rectangle(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t colour);
 
 #endif
