51ff58a368b5d2940d4207626e4e87134a62763b
ifknot <ifknot@hotmail.com>
2025-01-28 09:20:44 +0000

    Update hga_rectangle.c

--- src/HGA/hga_rectangle.c
+++ src/HGA/hga_rectangle.c
@@ -65,7 +65,6 @@ Z0:	    jcxz    J0                                          ; lhs and rhs share 
 		or      es:[di + bx], ah
 		and     es:[si + bx], dh
 		or      es:[si + bx], ah
-
 		// 7.1.2 work out fill 'colour'
 		mov 	al, colour
 		mov 	ah, al
@@ -80,14 +79,14 @@ Z1:     // 7.1.3 handle odd or even line lengths
 		stosb	                                             ; odd do one byte al 'colour'
 		xchg    di, si
 		stosb                                                ; odd byte colour lower line
-		jcxz    END
+		jcxz    VERT
 NC0:	// 7.1.4 remaining word(s) ax 'colour'
         mov     bx, cx
 		rep     stosw		                                 ; CX is checked for !=0 before even the first step
 		xchg    di, si
 		mov     cx, bx
 		rep     stosw
-		jmp 	END
+		jmp 	VERT
 J1:		// 7.2.0 special case same word (saves 48 clock cycles on 8086 line lengths 2 - 15)
         not     dx                                           ; convert proto-mask to mask word
         // 7.2.1 colour the shared lhs:rhs word                                       Clock Cycles
@@ -96,7 +95,7 @@ J1:		// 7.2.0 special case same word (saves 48 clock cycles on 8086 line lengths
 		xchg    di, si                                       ; swap lines
 		and     es:[di + bx], dx
 		or      es:[di + bx], ax
-		jmp 	END
+		jmp 	VERT
 J0:     // 7.3.0 special case same byte (saves 48 clock cycles on 8086 line lengths 0 - 7)
 		and     dl, dh                                      ; combine proto-mask into dl
 		not     dl		                                    ; convert proto-mask to mask
@@ -108,51 +107,55 @@ J0:     // 7.3.0 special case same byte (saves 48 clock cycles on 8086 line leng
 		and     es:[di + bx], dl
 		or      es:[di + bx], al
 
-END:    // draw verticle lines - use all the registers!
-		// 7. setup registers for rhs vline
+VERT:   // draw verticle lines - use all the registers!
+		// 1. setup registers for lhs & rhs vline
 		mov   	ah, 00000001                                ; AH lhs (proto)mask
 		mov     al, colour                                  ; AL lhs 'colour'
 		mov     dx, ax                                      ; DH rhs (proto)mask DL rhs 'colour'
 		mov		si, x			                           	; SI load x
 		mov     bx, w                                       ; BX load width
-        // 8. setup lhs pixel and mask
+        // 2.0 setup lhs pixel and mask
         mov		cx, si			                           	; CX copy of x
         and		cx, 7h			                           	; mask off 0111 lower bits i.e.mod 8 (thanks powers of 2)										; rotate mask bit by x mod 8
 		xor     cx, 7h                                      ; convert to bits to shift left
 	    shl		ax, cl			                           	; shift colour bit & proto-mask into position
 		not     ah                                          ; convert to mask
-		// setup rhs pixel and mask
+		// 2.1 setup rhs pixel and mask
 		mov		cx, si			                           	; CX copy of x
 		add     cx, bx                                      ; CX = x + w
-		dec     cx
+		dec     cx											; zero base width
         and		cx, 7h			                           	; mask off 0111 lower bits i.e.mod 8 (thanks powers of 2)										; rotate mask bit by x mod 8
-        xor     cx, 7h
-        shl     dx, cl
-        not     dh
-		// 9.0 convert x and w to column byte
+        xor     cx, 7h										; convert to bits to shift left
+        shl     dx, cl										; shift colour bit & proto-mask into position
+        not     dh											; convert to mask
+		// 3.0 reduce x to column byte
 	    shr		si, 1			                           	; calculate column byte x / 8
 	    shr		si, 1			                           	; poor old 8086 only has opcodes shifts by an implicit 1 or CL
 	    shr		si, 1
+		// 3.1 reduce w to column byte
+		shr     bx, 1										; w /8 
 		shr     bx, 1
 		shr     bx, 1
-		shr     bx, 1
-		// 10. setup y loop and lookup pointer
+		// 4. setup height loop counter 
 		mov 	cx, h                                      	; CX load height counter
 		dec     cx										    ; top and bottom pixels already in place hline
-		push    bp
-		mov     bp, y
-		inc     bp
-		shl     bp, 1
-		// 11. lhs & rhs pixels
+		// 5. setup y lookup index 
+		push    bp											; preserve BP for __asm use
+		mov     bp, y										; BP load y
+		inc     bp											; top line already drawn
+		shl     bp, 1										; convert to word index
+		// 6.0 look up row 
 L1:	    mov   	di, HGA_TABLE_Y_LOOKUP[bp]                  ; lookup y offset
+		// 6.1 plot lhs pixel
 		add   	di, si                                      ; add in x / 8
 		and		es:[di], ah								    ; mask lhs pixel
 		or 		es:[di], al									; or lhs 'colour'
+		// 6.2 plot rhs pixel
 		add     di, bx
 		and		es:[di], dh						            ; mask rhs pixel
 		or 		es:[di], dl						            ; or rhs 'colour'
 		add 	bp, 2 										; next line
-		loop 	L1
-		pop bp
+		loop 	L1											; repeat for height
+		pop bp												; restore BP so __asm can exit properly
     }
 }
