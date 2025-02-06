37d85213e6e5a2a04bddaefb76cf77b21df661f6
ifknot <ifknot@hotmail.com>
2025-02-06 11:11:12 +0000

    Update hga_filled_rectangle.c

--- src/HGA/hga_filled_rectangle.c
+++ src/HGA/hga_filled_rectangle.c
@@ -5,7 +5,7 @@
 void hga_filled_rectangle(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t colour) {
     __asm {
 		.8086
-		cld                                                  ; clear direction flag
+		cld                                                 ; clear direction flag
 		mov   	ax, vram_segment
 		mov   	es, ax
 		mov 	bx, x1										; BX load x1
@@ -24,21 +24,25 @@ void hga_filled_rectangle(uint16_t vram_segment, uint16_t x, uint16_t y, uint16_
 		shr		ax, 1			                           	; calculate column byte x2 / 8
 	    shr		ax, 1
 	    shr		ax, 1
+		// prepare registers before entering height loop
 		mov 	cx, ax
 		sub 	cx, bx										; CX line length (bytes)
-		mov 	si, y1										; SI load y1
-	    shl     si, 1                                       ; convert SI word pointer
-		mov   	di, HGA_TABLE_Y_LOOKUP[si]					; lookup y offset
-        mov 	al, colour
-		mov 	ah, al
+		mov 	si, cx										; SI copy of width
+        mov 	al, colour									; AL will select colour path
+		mov 	si, h										; SI height loop counter
+		push 	bp 											; preserve BP for __asm
+		mov 	bp, y										; BP load y1
+	    shl     bp, 1                                       ; convert BP word pointer
 		test 	al, al
 		jz 		BLK                                         ; branching to hard code 'colour' saves a few cycle
 		mov     ax, dx                                      ; proto-mask is white bits to 'colour'
 		
-WHT:    jcxz    J0                                          ; lhs and rhs share same byte?
+WHT:	jcxz    J0                                          ; lhs and rhs share same byte?
         dec     cx
         jcxz    J1                                          ; lhs and rhs share same word?
-        not 	dx											; convert proto-mask to mask word
+        not 	dx											; convert proto-mask to mask word	
+	
+L1:		mov   	di, HGA_TABLE_Y_LOOKUP[bp]					; lookup y offset
 		add 	di, bx										; have ES:DI point to lhs
 		and     es:[di], dl                            		; mask out target bits 	- 16 + EA(8)
 		or      es:[di], al                            		; colour target bits	- 16 + EA(8)
@@ -46,36 +50,20 @@ WHT:    jcxz    J0                                          ; lhs and rhs share 
 		inc 	di											; next byte
 		and     es:[di + bx], dh                            ; mask out target bits 	- 16 + EA(8)
 		or      es:[di + bx], ah                            ; colour target bits	- 16 + EA(8)
-		mov     ax, 0FFFFh                                   ; AX white
-		shr     cx, 1		                                 ; number of words to fill, lsb -> carry flag
-		jnc     NC1                                          ; even so no byte to fill
-		stosb	                                             ; odd do one byte al 'colour'
-		jcxz    END
-NC1:	rep     stosw		                                 ; CX is checked for !=0 before even the first step
-		loop 	WHT
-        jmp 	END
-	
-BLK:	jcxz    J0
-        dec     cx
-        jcxz    J1                                  	    ; lhs and rhs share same word?
-        not 	dx										 	; convert proto-mask to mask word
-        add 	di, bx										; have ES:DI point to lhs
-		and     es:[di], dl                            		; mask out target bits 	- 16 + EA(8)
-		or      es:[di], al                            		; colour target bits	- 16 + EA(8)
-		mov 	bx, cx										; rhs offset = length
-		inc 	di											; next byte
-		and     es:[di + bx], dh                            ; mask out target bits 	- 16 + EA(8)
-		or      es:[di + bx], ah                            ; colour target bits	- 16 + EA(8)
-		mov     ax, 0                                       ; AX black
+		// set up for string ops
+		mov     ax, 0FFFFh                                  ; AX white
 		shr     cx, 1		                                ; number of words to fill, lsb -> carry flag
-		jnc     NC0                                         ; even so no byte to fill
+		jnc     NC1                                         ; even so no byte to fill
 		stosb	                                            ; odd do one byte al 'colour'
-		jcxz    END
-NC0:	rep     stosw		                                ; CX is checked for !=0 before even the first step
-
-		loop 	BLK
-	
+		jcxz    NXT											; only 1 byte width
+NC1:	rep     stosw		                                ; CX is checked for !=0 before even the first step
+NXT		add 	bp, 2
+		xchg 	si, cx										; CX = height SI = width
+		//loop 	L1
         jmp 	END
+	
+BLK:	mov     ax, 0                                 		; AX black
+
 	
 J1:	    not     dx                                          ; convert proto-mask to mask word
         and     es:[di + bx], dx                            ; mask out target word 	- 16 + EA(8)
@@ -88,5 +76,5 @@ J0:     and     dl, dh                                      ; combine proto-mask
 		and     es:[di + bx], dl                            ; mask out target bits 	- 16 + EA(8)
 		or      es:[di + bx], al                            ; colour target bits	- 16 + EA(8)   
 
-END:
+END:	pop 	BP
 }
