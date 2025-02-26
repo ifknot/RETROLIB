32bc97c79687e7a1feb8175bfa218d845aadca37
ifknot <ifknot@hotmail.com>
2025-02-26 15:44:11 +0000

    bresenham first quadrant black and white

--- src/DEMOS/demo_bresenham.c
+++ src/DEMOS/demo_bresenham.c
@@ -14,12 +14,15 @@
 #include "../HGA/hga_bresenham_line.h"
 #include "../HGA/hga_rectangle.h"
 
+#include "../BIOS/bios_timer_io_services.h"
+
 #define ERR_GRAPHICS        "ERROR: No valid graphics adapter found!\n"
 #define ERR_GRAPHICS_INFO   "ERROR INFO: This version of %s requires an Hercules Graphics Adapter.\n"
 #define INFO_ADAPTER        "INFO: Adapter Type = %s\n"
 #define PRESS_ENTER         "Press <Enter>"
 
 int bresenham(int argc, char** argv) {
+    bios_ticks_since_midnight_t t1, t2;
     // 1. confirm appropriate graphics adapter presenddt
     uint8_t adapter_type = hga_detect_adapter();
     if (adapter_type < HW_VIDEO_ADAPTER_HGA) {
@@ -33,15 +36,38 @@ int bresenham(int argc, char** argv) {
     getchar();
     hga_graphics_mode();
     hga_select_display_buffer(HGA_BUFFER_1);
+    // white lines
     hga_cls(HGA_BUFFER_1);
     getchar();
-    //
-    //hga_bresenham_line(HGA_BUFFER_1, 100, 100, 200, 200, HGA_WHITE);
-    hga_bresenham_line(HGA_BUFFER_1, 100, 100, 200, 110, HGA_WHITE);
-    getchar();
+    bios_read_system_clock(&t1);
+    for(int j = 0; j < 20; ++j) {
+        int y = 0;
+        for(int i = 0; i < 50; ++i) {
+            hga_bresenham_line(HGA_BUFFER_1, 100, 100, 200, 100 + y, HGA_WHITE);
+            y +=2;
+        }
+    }
+    bios_read_system_clock(&t2);
+    printf("white ticks = %lu", t2 - t1);
     hga_rectangle(HGA_BUFFER_1, 100, 100, 100, 100, HGA_WHITE);
+    getchar();
+    // black lines
+    hga_fill_vram_buffer(HGA_BUFFER_1, 0xFF);
+    getchar();
+    bios_read_system_clock(&t1);
+    for(int j = 0; j < 20; ++j) {
+        int y = 0;
+        for(int i = 0; i < 50; ++i) {
+            hga_bresenham_line(HGA_BUFFER_1, 100, 100, 200, 100 + y, HGA_BLACK);
+            y +=2;
+        }
+    }
+    bios_read_system_clock(&t2);
+    printf("black ticks = %lu", t2 - t1);
+    hga_rectangle(HGA_BUFFER_1, 100, 100, 100, 100, HGA_BLACK);
     //  wait for ENTER key and switch back to text mode
     getchar();
     hga_text_mode();
+
     return 0;
 }
--- src/HGA/hga_bresenham_line.c
+++ src/HGA/hga_bresenham_line.c
@@ -5,11 +5,14 @@
 void hga_bresenham_line(uint16_t vram_segment, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t colour) {
     __asm {
 		.8086
-	    // 1. set up VRAM segment in ES
+	    // set up VRAM segment in ES
 	    mov   	ax, vram_segment
 		mov   	es, ax
-        // 2. set up registers
-        mov     ax, x1                                  ; AX = x1
+		// select hard code colour path
+		cmp     colour, 0
+		jz      BLACK
+        // set up registers
+WHITE:  mov     ax, x1                                  ; AX = x1
         sub     ax, x0                                  ; AX = dx = (x1 - x0)
         mov     si, y1                                  ; SI = y1
         sub     si, y0                                  ; SI = dy = (y1 - y0)
@@ -23,44 +26,89 @@ void hga_bresenham_line(uint16_t vram_segment, uint16_t x0, uint16_t y0, uint16_
         mov     ax, x0                                  ; AX is x
 
         // plot pixel
-PLOT:   push    dx                                      ; preserve corrupted registers
+WPLOT:   push    dx                                      ; preserve corrupted registers
         push    cx
-        push    bx
         push    ax
 
         mov     cx, ax                                  ; copy of x low order byte
         and     cx, 7                                   ; mask off 0111 lower bits ie x mod 8 (thanks powers of 2)
         xor     cx, 7                                   ; CL = number of bits to shift left (thanks bit flip XOR)
-       	mov   	dh, 00000001                            ; DH is (proto)mask byte
-		mov     dl, colour                              ; DL load 'colour'
+		mov     dx, 101h
 		shl		dx, cl			                        ; shift colour bit & proto-mask into position
 		not     dh                                      ; convert to mask
 		shr		ax, 1			                        ; calculate column byte x1 / 8
 	    shr		ax, 1			                        ; poor old 8086 only has opcodes shifts by an implicit 1 or CL
 	    shr		ax, 1
-	    shl     bx, 1                                   ; convert BX word pointer
+		shl     bx, 1                                   ; convert BX word pointer
 		mov   	di, HGA_TABLE_Y_LOOKUP[bx]
 		add     di, ax                                  ; ES:[DI] points to VRAM byte containing pixel location
+		shr     bx, 1
 		and		es:[di], dh								; mask out target pixel
 		or 		es:[di], dl							    ; or in the 'colour'
 
 		pop     ax
-		pop     bx
 		pop     cx
 		pop     dx                                      ; restore registers
-
 		// decision variable
 		cmp     dx, 0                                   ; if D > 0
-		jle     ELSE
+		jle     WELSE
 		inc     bx                                      ; y++
         add     dx, cx                                  ; D = D + (2 * (dy - dx))
-
-ELSE:   add     dx, si                                  ; D = D + 2*dy
-
-NEXT:   inc     ax
+WELSE:  add     dx, si                                  ; D = D + 2*dy
+        inc     ax
         cmp     ax, x1
-        jle     PLOT
-                              ; restore registers
+        jle     WPLOT
+        jmp     END
+
+        // set up registers
+BLACK:  mov     ax, x1                                  ; AX = x1
+        sub     ax, x0                                  ; AX = dx = (x1 - x0)
+        mov     si, y1                                  ; SI = y1
+        sub     si, y0                                  ; SI = dy = (y1 - y0)
+        mov     cx, si                                  ; CX = dy
+        sub     cx, ax                                  ; CX = (dy - dx)
+        shl     si, 1                                   ; SI = (2 * dy)
+        shl     cx, 1                                   ; CX = 2 * (dy - dx)
+        mov     dx, si                                  ; D = (2 * dy)
+        sub     dx, ax                                  ; D = (2 * dy) - dx
+        mov     bx, y0                                  ; BX is y
+        mov     ax, x0                                  ; AX is x
+
+        // plot pixel
+BPLOT:  push    dx                                      ; preserve corrupted registers
+        push    cx
+        push    ax
+
+        mov     cx, ax                                  ; copy of x low order byte
+        and     cx, 7                                   ; mask off 0111 lower bits ie x mod 8 (thanks powers of 2)
+        xor     cx, 7                                   ; CL = number of bits to shift left (thanks bit flip XOR)
+        mov     dh, 1
+		shl		dh, cl			                        ; shift colour bit & proto-mask into position
+		not     dh                                      ; convert to mask
+		shr		ax, 1			                        ; calculate column byte x1 / 8
+	    shr		ax, 1			                        ; poor old 8086 only has opcodes shifts by an implicit 1 or CL
+	    shr		ax, 1
+		shl     bx, 1                                   ; convert BX word pointer
+		mov   	di, HGA_TABLE_Y_LOOKUP[bx]
+		add     di, ax                                  ; ES:[DI] points to VRAM byte containing pixel location
+		shr     bx, 1
+		and		es:[di], dh								; mask out target pixel
+
+		pop     ax
+		pop     cx
+		pop     dx                                      ; restore registers
+		// decision variable
+		cmp     dx, 0                                   ; if D > 0
+		jle     BELSE
+		inc     bx                                      ; y++
+		add     dx, cx                                  ; D = D + (2 * (dy - dx))
+BELSE:  add     dx, si                                  ; D = D + 2*dy
+        inc     ax
+        cmp     ax, x1
+        jle     BPLOT
+
+END:
+
     }
 }
 
