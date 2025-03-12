e14c159e1d45bb7223867458ff4a1518b59198d7
ifknot <ifknot@hotmail.com>
2025-03-12 14:36:09 +0000

    Update test_lines.c

--- src/TESTS/test_lines.c
+++ src/TESTS/test_lines.c
@@ -1,6 +1,29 @@
 #include "test_lins.h"
 
+#define ERR_GRAPHICS        "ERROR: No valid graphics adapter found!\n"
+#define ERR_GRAPHICS_INFO   "ERROR INFO: This version of %s requires an Hercules Graphics Adapter.\n"
+#define INFO_ADAPTER        "INFO: Adapter Type = %s\n"
+#define PRESS_ENTER         "Press <Enter>"
+
 void test() {
-  printf("Bresenham line drawing performance tests\nPress Enter");
-  
+  printf("Bresenham line drawing performance tests Hercules Graphics cards\n");
+  // confirm appropriate graphics adapter present
+  uint8_t adapter_type = hga_detect_adapter();
+  if (adapter_type < HW_VIDEO_ADAPTER_HGA) {
+      fprintf(stderr, ERR_GRAPHICS);
+      fprintf(stderr, ERR_GRAPHICS_INFO, argv[0]);
+      return EXIT_FAILURE;
+  }
+  printf(INFO_ADAPTER, hw_video_adapter_names[adapter_type]);
+  // 2. wait for ENTER key and switch to HGA graphics mode
+  printf(PRESS_ENTER);
+  getchar();
+  hga_graphics_mode();
+  hga_select_display_buffer(HGA_BUFFER_1);
+  hga_cls(HGA_BUFFER_1);
+  getchar();
+
+  //  wait for ENTER key and switch back to text mode
+getchar();
+hga_text_mode();
 }
