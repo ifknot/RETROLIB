d16e6c66d57ba70873c71bad6496cf147f7615b8
ifknot <ifknot@hotmail.com>
2025-04-18 21:23:09 +0100

    centre box and knit lines ready for scroll test

--- src/DEMOS/demo_display_buffer.c
+++ src/DEMOS/demo_display_buffer.c
@@ -19,7 +19,7 @@ int display_buffer(int argc, char** argv) {
     uint16_t x, y, w, h;
     w = 8;
     h = w * 8;
-    x = (HGA_SCREEN_X_MAX / 2) - ((w / 2) * 8);
+    x = (HGA_BYTES_PER_LINE / 2) - (w / 2);
     y = (HGA_SCREEN_Y_MAX / 2) - (h / 2);
     // confirm appropriate graphics adapter present
     uint8_t adapter_type = hga_detect_adapter();
@@ -65,9 +65,15 @@ int display_buffer(int argc, char** argv) {
             hga_write_vram_buffer(HGA_BUFFER_1, col + 1, row, hga_read_vram_buffer(HGA_BUFFER_0, col, row));
         }
     }
-    /*
     getchar();
+    hga_knit_vram_buffer(HGA_BUFFER_1, 0, 255);
+    for(uint16_t row = 0; row < h; ++row) {
+        for(uint16_t col = 0; col < w; ++col) {
+            hga_write_vram_buffer(HGA_BUFFER_1, x + col, y + row, 255);
+        }
+    }
     // draw a cross hatch box read it and draw reversed inner box
+    /*
     getchar();
     // scroll the box around
     */
